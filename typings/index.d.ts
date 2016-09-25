d0680e72cd8611819e63078bb3ad81b573123f18
Michal Zalecki <michal@michalzalecki.com>
2016-09-25 11:44:40 +0200

    Obtain declarations through npm instead of typings

--- typings/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="globals/lodash/index.d.ts" />
-/// <reference path="globals/moment-node/index.d.ts" />
-/// <reference path="globals/moment/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/tape/index.d.ts" />
